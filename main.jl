e46ec2d7750769cb6b9d0ffbf0d9239dd485db59
nicolas1415 <nicolasbusch@sfr.fr>
2022-02-21 15:28:10 +0100

    main

---
+++ main.jl
@@ -0,0 +1,17 @@
+using Plots 
+include("input.jl")
+include("schemes.jl")
+
+function main()
+    U=Euler_Explicite(F_SIS,X₀,Δt,n,α,β)
+    S_SIS=[]
+    I_SIS=[]
+    for i in 1:n
+        push!(S_SIS,U[i][1])
+        push!(I_SIS,U[i][2])
+    end
+    plot(T,S_SIS,title="modèle SIS",label="sains",lw=3,yaxis="pourcentage de la poulation")
+    plot!(T,I_SIS,label="infectés",lw=3)
+    xlabel!("le temps en mois")
+end
+main()
