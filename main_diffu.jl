fcf74d619aa58811f708849aeb542955fcd772ea
Gaspard <gaspard.lodsg@gmail.com>
2022-03-15 14:10:13 +0100

    diffusion

--- main_diffu.jl
+++ main_diffu.jl
@@ -1,3 +1,15 @@
 using Plots 
 include("input_diffu.jl")
 include("schemes_diffu.jl")
+
+function main()
+    I=euler(F,α,β,i₀)
+    X=LinRange(0,L,nₓ)
+    p=plot(X)
+    for i in 1:10
+        plot!(p,X,I[i*10])
+    end
+    plot(p)
+end
+
+main()
--- schemes_diffu.jl
+++ schemes_diffu.jl
@@ -1,1 +1,20 @@
 include("input_diffu.jl")
+
+function F(X,α,β,x)
+    return β*(N-X[x])*X[x]-α*X[x]+D/(Δx*Δx)*(X[x+1]-2*X[x]+X[x-1])
+end
+
+function euler(F,α,β,i₀)
+    I=[i₀]
+    for t in 1:n
+        U=[0]*nₓ
+        for x in 2:(nₓ-1)
+            U[x]=I[t][x]+Δt*F(I[t],α,β,x)
+        end
+        U[1]=U[2]
+        U[nₓ]=U[nₓ-1]
+        #conditions Neuman
+        push!(I,U)
+    end
+    return I
+end
